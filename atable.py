de4a947be3b8505ad15fe59b02482b9fd1c4779a
Radomír Bosák <radomir.bosak@gmail.com>
2024-12-11 07:57:55 +0100

    Support sliding reset

--- atable.py
+++ atable.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from dataclasses import astuple, fields, is_dataclass
 from itertools import zip_longest
 import unicodedata
 
@@ -17,33 +18,71 @@ import unicodedata
 # 9. consider switching to tabulate
 
 
+def update_lens(old_lens, *cols, len_fn=len):
+    new_lens = [len_fn(col) for col in cols]
+    zips = zip_longest(old_lens, new_lens, fillvalue=0)
+    return [max(zp) for zp in zips]
+
+
 class ATable:
 
-    def __init__(self, max_row_lenght=88, delimiter=" ", asian_chars=True):
+    def __init__(self, max_row_lenght=88, delimiter="  ", asian_chars=True):
         self.col_lens = []
         self.delimiter = delimiter
+
+        # asian chars adjustment
         self.asian_chars = asian_chars
+        self.len = visual_len if self.asian_chars else len
+        self.ljust = (
+            fw_ljust if self.asian_chars else lambda text, length: text.ljust(length)
+        )
 
     def update_lens(self, *cols):
-        used_len = visual_len if self.asian_chars else len
-        new_lens = [used_len(col) for col in cols]
-        zips = zip_longest(self.col_lens, new_lens, fillvalue=0)
-        self.col_lens = [max(zp) for zp in zips]
+        self.col_lens = update_lens(self.col_lens, *cols, len_fn=self.len)
 
     def only_print(self, *cols):
         parts = []
         for col, length in zip(cols, self.col_lens):
-            adjusted = fw_ljust(col, length) if self.asian_chars else col.ljust(length)
-            parts.append(adjusted)
+            parts.append(self.ljust(col, length))
         print(self.delimiter.join(parts))
 
     def print(self, *cols):
+        if len(cols) == 1 and is_dataclass(cols[0]):
+            cols = astuple(cols[0])
         cols = [str(col) for col in cols]
         self.update_lens(*cols)
         self.only_print(*cols)
 
+    def print_header(self, *cols, header_separator="="):
+        if len(cols) == 1 and is_dataclass(cols[0]):
+            cols = [field.name for field in fields(cols[0])]
+        sep_row = [header_separator * len(col) for col in cols]
+        self.print(*cols)
+        self.print(*sep_row)
+
     def reset(self):
         self.col_lens = []
+
+
+class SlidingResetATable(ATable):
+    def __init__(self, *args, reset_window=100, **kwargs):
+        super().__init__(self, *args, **kwargs)
+        self.reset_window = reset_window
+        self.print_counter = 0
+        self.sliding_lens = []
+
+    def print(self, *cols):
+        super().print(*cols)
+        self.print_counter += 1
+
+        if self.print_counter % self.reset_window == 0:
+            self.col_lens = self.sliding_lens
+            self.sliding_lens = []
+
+    def update_lens(self, *cols):
+        super().update_lens(*cols)
+        self.sliding_lens = update_lens(self.sliding_lens, *cols, len_fn=self.len)
+
 
 
 def visual_len(text):
